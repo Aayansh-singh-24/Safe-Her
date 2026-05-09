c56b9526cc52bc64d87f76a29306e166c919ad71
Aayansh-singh-24 <luciferversion2.2@gmail.com>
2026-05-09 09:47:40 +0530

    SOS_integration

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from app.database import Base, engine
 from app.models.user import User
-from app.models.trsuted_contact import TrustedContacts
-from app.routes import trusted_contacts,alerts
+# from app.models.trsuted_contact import alert
+from app.routes import trusted_contacts,alert
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -11,7 +11,8 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="SafeHer Backend")
 
 app.include_router(trusted_contacts.router)
-app.include_router(alerts.router)
+app.include_router(alert.router)
+
 
 app.add_middleware(
     CORSMiddleware,
