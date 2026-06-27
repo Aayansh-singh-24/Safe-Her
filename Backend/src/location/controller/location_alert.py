33f94ea31141e1418de8f7f6a1e15e9958aacf05
Aayansh Singh <luciferversion2.2@gmail.com>
2026-06-27 23:29:26 +0530

    twillio setup

--- Backend/src/location/controller/location_alert.py
+++ Backend/src/location/controller/location_alert.py
@@ -1,15 +1,38 @@
-from fastapi import HTTPException,status, BackgroundTasks
+from fastapi import HTTPException,status
 from sqlalchemy.orm import Session
 from src.location.schema.dtos import locationAlertSchema
 from src.trusted_contact.models.model import TrustedContactsModel
 from src.user.models import UserModel
-
+import twilio
 # Twillio setup
-def send_sms(contact:TrustedContactsModel, message:str):
-    pass
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+PHONE = os.getenv("TWILIO_PHONE_NUMBER")
+from twilio.rest import Client
 
 
-def alert(location:locationAlertSchema, backgroud_task:BackgroundTasks, db:Session, user:UserModel):
+
+# print(message.sid)
+# def send_sms(phoneNo:TrustedContactsModel, message:str):
+def send_sms(phone_no:str,message_body:str):
+    try:
+     client = Client(ACCOUNT_SID, AUTH_TOKEN)
+     message = client.messages.create(
+     body=message_body,
+     from_=PHONE,
+     to=phone_no
+     )
+     print(f"SMS successfully sent to {phone_no}")
+    except Exception as e:
+        print(f"Failed to send SMS to {phone_no}: {str(e)}")
+
+
+def alert(location:locationAlertSchema, db:Session, user:UserModel):
     Contacts = db.query(TrustedContactsModel).filter(
         user.id == TrustedContactsModel.userId
     ).all()
@@ -20,15 +43,22 @@ def alert(location:locationAlertSchema, backgroud_task:BackgroundTasks, db:Sessi
 
     latitude = location.latitude
     longitude  = location.longitude
+    custom_msg = location.message or "I need Help!!"
 
     location_link = (f"https://www.google.com/maps/search/?api=1&query={latitude},{longitude}")
 
     for contact in Contacts:
-        message = f"""
-                Emercengy Alert
-                I need Help
+        message_body = f"""
+                Emercengy Alert!
+                \n{custom_msg}\n
                 Live location : {location_link}"""
-        backgroud_task.add_task(send_sms,contact,message)
+        country_code = contact.country_code or "+91"
+        if not country_code_str.startswith("+"):
+            country_code_str = "+" + country_code_str
+        phone_no = f"{country_code}{contact.phoneNo}"
+        
+        send_sms(phone_no, message_body)
+
+        # send_sms(contact,message)
 
     return {"link" : location_link}
-
